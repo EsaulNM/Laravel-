53a29fcd5843513b8255e13f26a14ae2d52f6ae2
EsaulNM <esaulnm@outlook.com>
2022-11-17 12:31:10 -0600

    3.5 Funciones de visualización

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,10 +34,13 @@
             <p>Aqui se mostraran la lista de las peliculas</p>
         </div>
         <div class="container p-5 my-5 bg-dark text white">
-            <p>Espacio para las peliculas</p>
+            <p>Espacio para las peliculas.</p>
         </div>
         <div class="container p-5 my-5 bg-dark text white">
-            <p>Espacio para las peliculas</p>
+            <p>Espacio para las peliculas.</p>
+        </div>
+        <div class="container p-5 my-5 bg-dark text white">
+            <p>Espacio para las peliculas.</p>
         </div>
 
 </div>
