c08aa281fa1de1108f6a3e4ce40109e2cdd6d11b
EsaulNM <esaulnm@outlook.com>
2022-11-25 14:24:37 -0600

    Producto Integrador

--- angular_movies/src/app/app.component.ts
+++ angular_movies/src/app/app.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  movies = [];
   title = 'angular_movies';
+
+  constructor() {
+    // localhost:8000/api/movies
+    const url = 'http://127.0.0.1:8000/api/movies';
+
+    this.getMovies(url);
+  }
+
+  async getMovies(url: string) {
+    fetch(url)
+    .then( response => {
+      return response.json()
+    })
+    .then(data => {
+      this.movies = data
+      console.log(this.movies)
+    })
+  }
 }
